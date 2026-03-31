5c79ebfa67d6fbec5a40ff81c9a0333046dd8e6a
bossking2731016033-ship-it <bossking2731016033@gmail.com>
2026-03-31 06:59:55 +0000

    update api route

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("Backend is working 🚀");
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
   });
+
+  app.get("/api", (req, res) => {
+    res.json({ message: "API is working 🚀" });
+    });
+
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+      console.log("Server running");
+      });
